5ec94aaf61ebd9e083adecdd3e83736aec964f21
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-10 14:57:17 -0300

    Organizando as rotas

--- gatibook/src/app/home/home.module.ts
+++ gatibook/src/app/home/home.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
+  declarations: [HomeComponent],
   imports: [CommonModule, HomeRoutingModule],
   exports: [HomeComponent],
 })
