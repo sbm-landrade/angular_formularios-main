4a024f9f9b1501310d7a2764411bebf8429fe228
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-10 15:21:54 -0300

    Implementando formulário Login na página Home

--- gatibook/src/app/home/home-routing.module.ts
+++ gatibook/src/app/home/home-routing.module.ts
@@ -1,15 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{
-  path: '',
-  component: HomeComponent
-},
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
