03739368aa60a20e6257068013fe3ec29d5ac0d1
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-11 13:08:03 -0300

    Criando componentes/mensagem

---
+++ gatibook/src/app/componentes/mensagem/mensagem.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MensagemComponent } from './mensagem.component';
+
+
+
+@NgModule({
+  declarations: [MensagemComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MensagemModule { }
