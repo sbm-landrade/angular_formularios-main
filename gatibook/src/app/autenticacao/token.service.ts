8d34c651d6bb8fb6c97fd2ecdea1aac670c082a2
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-12 16:28:41 -0300

    Token

--- gatibook/src/app/autenticacao/token.service.ts
+++ gatibook/src/app/autenticacao/token.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
+const KEY = 'token';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
-  constructor() { }
+  retornaToken() {
+    return localStorage.getItem(KEY) ?? '';
+  }
+
+  salvaToken(token:string) {
+    localStorage.setItem(KEY, token);
+  }
+  excluirToken() {
+    localStorage.removeItem(KEY);
+  }
+  possuiToken() {
+    return !!this.retornaToken();
+  }
 }
