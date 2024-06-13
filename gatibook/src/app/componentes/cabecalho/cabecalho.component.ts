4e422d3c3e04b94f0f3dc04f1f1c92d78b9630ee
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-13 10:26:54 -0300

    Cabeçalho

--- gatibook/src/app/componentes/cabecalho/cabecalho.component.ts
+++ gatibook/src/app/componentes/cabecalho/cabecalho.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 
 @Component({
   selector: 'app-cabecalho',
   templateUrl: './cabecalho.component.html',
   styleUrls: ['./cabecalho.component.css']
 })
-export class CabecalhoComponent implements OnInit {
+export class CabecalhoComponent {
 
-  constructor() { }
+  user$ = this.usuarioService.retornaUsuario();
 
-  ngOnInit(): void {
+  constructor(private usuarioService: UsuarioService, private router: Router) { }
+
+  logout() {
+    this.usuarioService.logout();
+    this.router.navigate(['']);
   }
-
 }
